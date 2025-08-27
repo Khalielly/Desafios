abbc78412e90bee31b9fa3848120b1924676a303
Danielly Khalil <152915978+Khalielly@users.noreply.github.com>
2025-08-27 10:39:34 -0300

    formatando o password eye

--- Desafio1/assets/js/app.js
+++ Desafio1/assets/js/app.js
@@ -7,3 +7,22 @@ document.getElementById('cpf').addEventListener('input', function(e) {
                           .replace(/(-\d{2})\d+?$/, '$1');
     e.target.value = cpfPattern;
   });
+
+  const passwordIcons = document.querySelectorAll('.password-icon');
+
+  passwordIcons.forEach(icon => {
+      icon.addEventListener('click', function () {
+          const input = this.previousElementSibling;
+  
+          if (input.type === 'password') {
+              input.type = 'text';
+              this.classList.remove('fa-eye-slash');
+              this.classList.add('fa-eye');
+          } else {
+              input.type = 'password';
+              this.classList.remove('fa-eye');
+              this.classList.add('fa-eye-slash');
+          }
+      });
+  });
+  
